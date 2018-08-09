c239322b1f3396a0bfbf85659147df95ea086bbe
siturra <iturra.sebastian@hotmail.com>
2018-08-09 19:17:36 -0400

    first commit

---
+++ CmsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Siturra\Cms;
+
+use Illuminate\Support\ServiceProvider;
+
+class CmsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
