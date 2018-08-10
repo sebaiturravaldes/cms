bdd303170a46cdd8547f51b58d660dffcb78ebce
siturra <iturra.sebastian@hotmail.com>
2018-08-09 20:06:40 -0400

    modify composer

--- src/CmsServiceProvider.php
+++ src/CmsServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace Cms;
 
-use Illuminate\Support\ServiceProvider;
+//use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class CmsServiceProvider extends ServiceProvider
 {
